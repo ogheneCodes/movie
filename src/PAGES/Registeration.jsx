f7dda08087c080fafbc6a7e943c7eaf60ff3afaf
maxbobo <oghenemaga.ebobo@unn.edu.ng>
2022-05-19 22:58:20 +0100

    registration

--- src/PAGES/Registeration.jsx
+++ src/PAGES/Registeration.jsx
@@ -4,27 +4,37 @@ import '../CSS/Registration.css'
 function Registeration() {
   return (
     <>
-        <main>
-            <h1>Register Here</h1>
-            <form>
-                <label htmlFor='firstname'> First Name</label>
-                <input type='text' name='firstname' />
-                <label htmlFor='lastname'> Last Name</label>
-                <input type='text' name='lastname'/>
-                <br />
-                <label htmlFor='email'> Email</label>
-                <input type='email' name='email'/>
-                <label htmlFor='phone'> Phone Number</label>
-                <input type='text' name='phone'/>
-                <br />
-                <label htmlFor='address'> Address</label>
-                <input type='address' name='address'/>
-                <label htmlFor='userType'> User Type</label>
-                <input type='number' name='userType'/>
-                <br />
-                <input type="submit"/>
-            </form>
-        </main>
+    <div class="container">
+    <div class="row justify-content-center">
+    <div class="col-md-5">
+     <div class="card">
+       <h2 class="card-title text-center">Register <a href="http://opensnippets.com">open snippets</a></h2>
+        <div class="card-body py-md-4">
+         <form>
+            <div class="form-group">
+               <input type="text" class="form-control" id="name" placeholder="Name" />
+          </div>
+          <div class="form-group">
+               <input type="email" class="form-control" id="email" placeholder="Email" />
+                              </div>
+                              
+                            
+     <div class="form-group">
+       <input type="password" class="form-control" id="password" placeholder="Password" />
+     </div>
+     <div class="form-group">
+        <input type="password" class="form-control" id="confirm-password" placeholder="confirm-password" />
+     </div>
+     <div class="d-flex flex-row align-items-center justify-content-between">
+        <a href="#">Login</a>
+        <button class="btn btn-primary">Create Account</button>
+            </div>
+         </form>
+       </div>
+    </div>
+  </div>
+  </div>
+  </div>  
     </>
   )
 }
